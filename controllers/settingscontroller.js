92471d26459a677e72c631c9c7a98f1e56807587
Adrian Blanco <adrianblp@gmail.com>
2014-03-10 15:17:15 +0100

    ICE

--- controllers/settingscontroller.js
+++ controllers/settingscontroller.js
@@ -1,3 +1,13 @@
+var mongoose = require('mongoose');
+var userModel = require('../models/UserModel');
+mongoose.connect('mongodb://localhost/oneword');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error when opening database'));
+db.once('open', function callback () {
+//Do nothing special here
+});
+var users = mongoose.model('Users', userModel);
+
 module.exports.controller = function(app) {
   app.get('/settings', function(req, res) {
     res.render('settings', {
@@ -5,5 +15,21 @@ module.exports.controller = function(app) {
       ,description : 'Account settings'
       ,author : 'adrianblp, cwinsnes, robineng'
     });
+
+    if(!req.session.user) {
+      res.redirect('/login');
+    }
+  });
+
+//req.session.user är satt
+//req.session.user.color
+  app.post('/settings/color', function(req, res) {
+    if(req.session.user && /[0-9A-F]{6}$/i.test(req.body.user.color)){
+      users.update(
+        {name : req.session.username},
+        {$set: {'color' : '#' + req.body.user.color}});
+      req.session.user.color = req.body.user.color;
+    }
+    res.redirect('/settings');
   });
 }
