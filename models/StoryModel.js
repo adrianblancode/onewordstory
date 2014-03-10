d77a05922e1a1b3ec02534a0201cdee86c9cc97f
Robin Engström <robineng91@gmail.com>
2014-03-10 19:37:03 +0100

    story title now unique

--- models/StoryModel.js
+++ models/StoryModel.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose'),
     ObjectId = Schema.ObjectId;
 
 var storySchema = new Schema({
-    title : { type: String, required: true},
+    title : { type: String, required: true, unique: true},
     admin : { type: Schema.ObjectId, ref: 'UserModel', required: true}
 });
 
