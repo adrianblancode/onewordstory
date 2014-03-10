14d5ac7c9fc5519b59b5c3ccc772fe92827e55dc
Robin Engström <robineng91@gmail.com>
2014-03-10 17:29:28 +0100

    can now create story and mainstory auto-made

--- models/StoryModel.js
+++ models/StoryModel.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose'),
     ObjectId = Schema.ObjectId;
 
 var storySchema = new Schema({
-    storyid : {type : Number, required : true},
     title : { type: String, required: true},
     admin : { type: Schema.ObjectId, ref: 'UserModel', required: true}
 });
